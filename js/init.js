aaa0d0375d7e09efa7a9bb64d7f7629e48dbe592
sugiyamaoutcomeslab <sugiyama.outcomes.lab@gmail.com>
2016-05-27 21:28:56 +0000

    Fixed scrollup button

--- js/init.js
+++ js/init.js
@@ -15,18 +15,16 @@ jQuery(document).ready(function() {
   /*----------------------------------------------------*/
   /*	Back To Top Button
   /*----------------------------------------------------*/
-  var pxShow = 300; //height on which the button will show
-  var fadeInTime = 400; //how slow/fast you want the button to show
-  var fadeOutTime = 400; //how slow/fast you want the button to hide
+  var pxShow = 200; //height on which the button will show
   var scrollSpeed = 300; //how slow/fast you want the button to scroll to top. can be a value, 'slow', 'normal' or 'fast'
 
   // Show or hide the sticky footer button
   jQuery(window).scroll(function() {
     if (jQuery(window).scrollTop() >= pxShow) {
-      jQuery("#go-top").fadeIn(fadeInTime);
+      jQuery("#go-top").fadeIn();
     }
     else {
-      jQuery("#go-top").fadeOut(fadeOutTime);
+      jQuery("#go-top").fadeOut();
     }
   });
 
